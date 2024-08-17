baab9ba63371c7e9acae1a9b588e8a3f1b55be8d
Mmari00 <1software.files1@gmail.com>
2024-08-17 17:23:20 -0500

    primeros cambios

--- datos/src/main/java/com/example/Main.java
+++ datos/src/main/java/com/example/Main.java
@@ -3,5 +3,22 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        //Declaración de variables
+        int edad = 17;
+        short codigoPostal = 12345;
+        double salario = 3500.00;
+        String nombre = "Juan Pérez";
+        boolean esMayorDeEdad = true;
+
+
+        //Clases envolventes
+
+        //Casteo de las variables
+        int entero = (int) salario;
+        System.out.println(salario);
+        
+
+
     }
 }
