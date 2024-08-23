9f1cfb6bb7eb1a028e31176c30bdc0e25bc0a7f4
Mmari00 <1software.files1@gmail.com>
2024-08-22 21:23:11 -0500

    casteo y parseo de variables

--- datos/src/main/java/com/example/Main.java
+++ datos/src/main/java/com/example/Main.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
 
         //Declaración de variables
         int edad = 17;
@@ -10,14 +10,24 @@ public class Main {
         double salario = 3500.00;
         String nombre = "Juan Pérez";
         boolean esMayorDeEdad = true;
+        String numeroStr = "12345678";
+        String decimalStr = "3.14159";
 
 
         //Clases envolventes
 
         //Casteo de las variables
-        int entero = (int) salario;
-        System.out.println(salario);
-        
+        int enteroSala = (int) salario;
+        System.out.println("Variable 'salario' pasada a entero: " + enteroSala);
+        long largoCodigo = (long) codigoPostal;
+        System.out.println("Variable 'codigoPostal' pasada a long: " + largoCodigo);
+        System.out.println("¿Qué sucede con la información al realizar el casteado? R/= Cuando realizamos el casteado la infomación cambiará de su estado de presentación, o sea, si al principio se tenía un tipo de variable con el dato; al aplicar el casteado se cambia el tipo de variable al dato que se desea cambiar.");
+
+        //Parseo de las variables
+        int numeroInt = Integer.parseInt(numeroStr);
+        System.out.println("Variable 'numeroStr' pasada a entero: " + numeroInt );
+        double decimalValor= Double.parseDouble(decimalStr);
+        System.out.println("Variable 'decimalStr' pasada a double: " + decimalValor);
 
 
     }
